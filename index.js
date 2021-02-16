304ed097ff84dc93fdcb89ef5b09cb44291fad46
RITWICK <ritz6580@gmail.com>
2021-02-16 12:58:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const strings=['a','b','c','d','e']
+strings.push('f')
+console.log(strings)
+strings.pop()
+console.log(strings)
+strings.unshift('x')
+console.log(strings)
+strings.splice(2,0,'vrinda')
+console.log(strings)
